a99915d473fd2f8e20f51c781bb2363782a4f7c9
okonomi <oknm@okonomi.sakura.ne.jp>
2017-12-11 22:54:55 +0900

    Modify application name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
-set :application, "webpacker-sample"
+set :application, "rals-webpacker-sample"
 set :repo_url, "."
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/vagrant/src/local/rails/webpacker-sample"
+set :deploy_to, "/home/vagrant/src/local/rails/rails-webpacker-sample"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
