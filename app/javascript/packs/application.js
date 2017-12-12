417b3668afbeaef87e9ad87b4e3425c030f322de
okonomi <oknm@okonomi.sakura.ne.jp>
2017-12-13 02:34:41 +0900

    Separate component definition

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,13 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import LikeButton from '../src/components/like_button'
+
+document.addEventListener('turbolinks:load', () => {
+  const elem = document.getElementById('like-button');
+  if (elem) {
+    ReactDOM.render(<LikeButton />, elem)
+  }
+})
--- app/javascript/src/components/like_button.jsx
+++ app/javascript/src/components/like_button.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import FontAwesome from 'react-fontawesome'
 import faStyles from 'font-awesome/scss/font-awesome.scss';
 
-class LikeButton extends React.Component {
+export default class LikeButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -19,10 +18,3 @@ class LikeButton extends React.Component {
     );
   }
 }
-
-document.addEventListener('turbolinks:load', () => {
-  const elem = document.getElementById('like-button');
-  if (elem) {
-    ReactDOM.render(<LikeButton />, elem)
-  }
-})
